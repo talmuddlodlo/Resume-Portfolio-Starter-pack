5aa31064aea37505ca98501de9062ec6e334e05a
Talmud <talmuddlodlo88@gmail.com>
2024-07-30 16:23:49 +0200

    title

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              <a title="Talmud" href="https://talmud-portfolio.vercel.app/">
                 Talmud
               </a>
             </li>
